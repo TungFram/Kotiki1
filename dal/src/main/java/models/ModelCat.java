94e544c29ac1b8690e2883f4d9c7d77574936aba
vladimir.sidortsov <tungfram@yandex.ru>
2022-04-15 04:39:47 +0300

    models

---
+++ dal/src/main/java/models/ModelCat.java
@@ -0,0 +1,57 @@
+﻿package models;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Value
+@Builder(builderClassName = "CatBuilder",
+        builderMethodName = "createBuilder",
+        toBuilder = true,
+        access = AccessLevel.PRIVATE,
+        setterPrefix = "with")
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "cat")
+public class ModelCat {
+    
+    @Id
+    @SequenceGenerator(name = "pet_seq_gen", sequenceName = "pet_sequence", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pet_seq_gen")
+    @Column(name = "id", nullable = false)
+    int id;
+
+    @EqualsAndHashCode.Exclude
+    @Column(name = "name")
+    String name;
+    
+    @Column(name = "date_birth")
+    @Builder.Default LocalDate dateOfBirth = LocalDate.now();
+    
+    @Column(name = "id_of_type")
+    int idOfType;
+    
+    @Column(name = "id_of_color")
+    int idOfColor;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "cats")
+    @Builder.Default List<ModelOwner> owners = new ArrayList<>();
+    
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @JoinTable(name = "friendship",
+            joinColumns = @JoinColumn(name = "id_of_first_cat", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_of_second_cat", referencedColumnName = "id")) //todo: Можно ли так делать
+    @Builder.Default List<ModelCat> fiends = new ArrayList<>();
+}
---
+++ dal/src/main/java/models/ModelOwner.java
@@ -0,0 +1,46 @@
+﻿package models;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Value
+@Builder(builderClassName = "OwnerBuilder",
+        builderMethodName = "createBuilder",
+        toBuilder = true,
+        access = AccessLevel.PRIVATE,
+        setterPrefix = "with")
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "owner")
+public class ModelOwner {
+
+    @Id
+    @SequenceGenerator(name = "owner_seq_gen", sequenceName = "own_sequence", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "owner_seq_gen")
+    @Column(name = "id", nullable = false)
+    int id;
+    
+    @Column(name = "name")
+    String name;
+    
+    @Column(name = "surname")
+    String surname;
+    
+    @Column(name = "DateOfBirth")
+    LocalDate DateOfBirth;
+
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @JoinTable(name = "affiliation",
+            joinColumns = @JoinColumn(name = "id_of_owner", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_of_cat", referencedColumnName = "id"))
+    List<ModelCat> cats;
+}
